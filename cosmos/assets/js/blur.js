0800306eb052730ffbb4e38209ad716610381208
Rapid Coder <codbegtocodpro@gmail.com>
2023-08-23 21:44:01 +0530

    loaders updated

--- cosmos/assets/js/blur.js
+++ cosmos/assets/js/blur.js
@@ -1,13 +1,14 @@
 
 const loader1=document.getElementById('loading')
 const blob=document.getElementById("blob2")
+const teams=document.getElementById("teams")
+setTimeout(()=>{
+    blob.style.width="1000%"
+},9500)
 
 setTimeout(()=>{
-    blob.style.width="200%"
-},6500)
+   loader1.style.opacity="0"
+   loader1.style.zIndex="0"
+   teams.style.overflowY="visible"
+},10000)
 
-setTimeout(()=>{
-    loader1.style.opacity="0"
-    loader1.style.zIndex="0"
-    
-},7000)
--- cosmos/assets/js/blur3.js
+++ cosmos/assets/js/blur3.js
@@ -1,23 +1,13 @@
 const loader3=document.getElementById('loading2')
 const blob=document.getElementById("blob1")
-
+const home=document.getElementById("home")
 setTimeout(()=>{
-    blob.style.width="200%"
+    blob.style.width="1000%"
 },4500)
 setTimeout(()=>{
     
     loader3.style.opacity="0"
     loader3.style.zIndex="0"
-    
+    home.style.overflowY="visible"
     
 },5000)
-document.addEventListener("wheel",()=>{
-    if (window.scrollY>=600){
-        console.log(window.scrollY)
-        document.getElementById("nav").classList.add("light-bg")
-    }
-    else{
-        console.log(window.scrollY)
-        document.getElementById("nav").classList.remove("light-bg")
-    }
-})
